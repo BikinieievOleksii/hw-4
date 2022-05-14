f91d9997d94c820ac2c7004f8de54897dbe6af49
Oleksii Bikinieiev <Tirexys@gmail.com>
2022-05-14 11:42:02 +0300

    modified ControlPanelTest

--- ControlPanel.java
+++
@@ -1,9 +0,0 @@
-class ControlPanel {
-    private Altimeter altimeter = new Altimeter();
-    private AirGauge airGauge = new AirGauge();
-
-    public void control() {
-        System.out.println(altimeter.toString());
-        System.out.println(airGauge.toString());
-    }
-}

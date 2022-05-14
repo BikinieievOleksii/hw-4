51c2fad33f78d43b63dcb1eab540ac70c228bb9e
Oleksii Bikinieiev <Tirexys@gmail.com>
2022-05-14 11:36:46 +0300

    Created hm-4

---
+++ ControlPanel.java
@@ -0,0 +1,9 @@
+class ControlPanel {
+    private Altimeter altimeter = new Altimeter();
+    private AirGauge airGauge = new AirGauge();
+
+    public void control() {
+        System.out.println(altimeter.toString());
+        System.out.println(airGauge.toString());
+    }
+}
